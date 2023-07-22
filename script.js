5126277d1e7b49da1b5a58272d8eee515b17982e
CC Home <127169630+PythJav@users.noreply.github.com>
2023-07-22 11:28:15 +0700

    Add multiple operation capability

--- script.js
+++ script.js
@@ -19,36 +19,36 @@ const calculate=(n1,operator,n2)=>{
  else if (operator ==='divide'){
   result = parseFloat(n1)/parseFloat(n2);
  }
- return result;
+ return Math.round(result*10)/10;
  }
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
+// keys.addEventListener('click', e => {
+//     if (e.target.matches('button')) {
 
-    const key = e.target;
-    const action = key.dataset.action;
-    if (!action) {
-        console.log('number key!')
-      }
-      if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-      ) {
-        console.log('operator key!')
-      }
-      if(action ==='clear'){
-        console.log('clear key')
-      }
-      if(action ==='decimal'){
-        console.log('decimal key')
-      }
-      if(action ==='calculate'){
-        console.log('equal key')
-      }
+//     const key = e.target;
+//     const action = key.dataset.action;
+//     if (!action) {
+//         console.log('number key!')
+//       }
+//       if (
+//         action === 'add' ||
+//         action === 'subtract' ||
+//         action === 'multiply' ||
+//         action === 'divide'
+//       ) {
+//         console.log('operator key!')
+//       }
+//       if(action ==='clear'){
+//         console.log('clear key')
+//       }
+//       if(action ==='decimal'){
+//         console.log('decimal key')
+//       }
+//       if(action ==='calculate'){
+//         console.log('equal key')
+//       }
 
-    }})
+//     }})
 
 
 
@@ -68,6 +68,7 @@ keys.addEventListener('click', e => {
         if(!action){
             if (displayedNum==='0'||lastKey==='operator'){
                 display.textContent=keyContent
+                calculator.dataset.lastKey='num';
             }
             else{
                 display.textContent=displayedNum+keyContent
@@ -84,7 +85,11 @@ keys.addEventListener('click', e => {
 
           calculator.dataset.lastKey='decimal';
           }
+          if(action==='clear'){
+            display.textContent='';
+            calculator.dataset.lastKey="";
 
+          }
 
             
         
@@ -97,21 +102,43 @@ keys.addEventListener('click', e => {
             action ==='subtract'||
             action ==='multiply'||
             action ==='divide')  {
+
+              if(calculator.dataset.moreCal==='more'){
+          let secondVal = displayedNum;
+          let firstVal= calculator.dataset.firstVal;
+          let operator =calculator.dataset.operator;
+          display.textContent= calculate(firstVal,operator,secondVal);
+          calculator.dataset.firstVal= display.textContent;
+
+              }
+
+            else{
                 calculator.dataset.firstVal= displayedNum;
                 calculator.dataset.operator=action;
                 key.classList.add('is-depressed');
                 calculator.dataset.lastKey = 'operator';
-    
+                calculator.dataset.moreCal='more';
+              
+              }
             }   
 
 
-        if (action==='calculate'){
+        if (action==='calculate'&&calculator.dataset.lastKey!='calculate'){
+
           const secondVal = displayedNum;
           const firstVal= calculator.dataset.firstVal;
           const operator =calculator.dataset.operator;
+          if(calculator.dataset.lastKey='divide'&& display.textContent==='0'){
+            display.textContent="Ayy what you playing";
+      
+          }
+          else{
 
          display.textContent= calculate(firstVal,operator,secondVal);
          calculator.dataset.lastKey='calculate';
+         calculator.dataset.moreCal='';
+
+        }
          
         }
         
